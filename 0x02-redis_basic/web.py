5e3c4f822a6981bc55c9edadf92899f6500b1a9c
Ngobidi <chibuzormaxwell@gmail.com>
2024-05-15 06:28:37 -0700

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python3
-'''validate a module with tools for caching and tracking.
+'''validates A module with tools for caching and tracking.
 '''
-import requests
 import redis
-import time
+import requests
 from functools import wraps
- 
-redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
- 
-def cached_and_tracked(expires=10):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            cache_key = f"cache:{url}"
-            count_key = f"count:{url}"
- 
-            '''validates if the output is cached.
-            '''
-            cached_result = redis_conn.get(cache_key)
-            if cached_result:
-                redis_conn.incr(count_key)
-                return cached_result.decode('utf-8')
- 
-            '''Find the page and cache the output.
-            '''
-            response = func(*args, **kwargs)
-            redis_conn.setex(cache_key, expires, response)
- 
-            '''Track the URL access_count.
-            '''
-            redis_conn.incr(count_key)
- 
-            return response
-        return wrapper
-    return decorator
- 
-def get_page(url):
-    response = requests.get(url)
-    return response.text
+from typing import Callable
+
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the result of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for cache result.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Result the content of a URL after cache the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
