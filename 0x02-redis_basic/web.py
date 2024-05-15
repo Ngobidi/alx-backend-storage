400d3df83044c7d72620007df0dfdc5df852a2dd
Ngobidi <chibuzormaxwell@gmail.com>
2024-05-15 06:24:40 -0700

    new update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 '''validate a module with tools for caching and tracking.
 '''
-import redis
 import requests
+import redis
+import time
 from functools import wraps
-from typing import Callable
-
-
-redis_store = redis.Redis()
-'''The module_level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the result of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the outputs.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
-
-
-@data_cacher
-def get_page(url: str) -> str:
-    '''Results the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+ 
+redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+ 
+def cached_and_tracked(expires=10):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            url = args[0]
+            cache_key = f"cache:{url}"
+            count_key = f"count:{url}"
+ 
+            '''validates if the output is cached.
+            '''
+            cached_result = redis_conn.get(cache_key)
+            if cached_result:
+                redis_conn.incr(count_key)
+                return cached_result.decode('utf-8')
+ 
+            '''Find the page and cache the output.
+            '''
+            response = func(*args, **kwargs)
+            redis_conn.setex(cache_key, expires, response)
+ 
+            '''Track the URL access_count.
+            '''
+            redis_conn.incr(count_key)
+ 
+            return response
+        return wrapper
+    return decorator
+ 
+def get_page(url):
+    response = requests.get(url)
+    return response.text
